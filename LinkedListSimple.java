b796ae81393fa4863083aa2c1a7421e034a99834
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 18:40:23 +0530

    Created simple linked list

--- LinkedListSimple.java
+++ LinkedListSimple.java
@@ -2,5 +2,26 @@ package com.bridgelab;
 
 public class LinkedListSimple
 {
+	Node head;
 	
+	public void insert(int data)
+	{
+		Node newNode = new Node();
+		newNode.data = data;
+		newNode.next = null;
+		
+		if(head == null)
+		{
+			head = newNode;
+		}
+		else
+		{
+			Node tempNode = head;
+			while(tempNode.next != null)
+			{
+				tempNode = tempNode.next;
+			}
+			tempNode = newNode;
+		}
+	}
 }
