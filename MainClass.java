274c44f0cf1ce6edfdd286d37994282abd952fb5
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 18:07:07 +0530

    Display: Welcome Message.

--- MainClass.java
+++ MainClass.java
@@ -4,6 +4,6 @@ public class MainClass
 {
 	 public static void main(String[] args)
 	 {
-		 System.out.println("Welcome to simple linked list program.");
+		 System.out.println("Welcome to simple linked list program");
 	 }
 }
