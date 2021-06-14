88c12ab35a78a68d704f608d9bc395e9fba28648
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-14 21:55:45 +0530

    Passing element fo find function that we want to search in linkedlist and if found printing appropriate message.

--- MainClass.java
+++ MainClass.java
@@ -10,7 +10,16 @@ public class MainClass
 		 linkedListObejct.insertAtLast(30);
 		 linkedListObejct.insertAtLast(70);
 		 
-		 linkedListObejct.deleteAt(2);
 		 linkedListObejct.printList();
+		 
+		 //Searching for the element in linkedlist if found printing message else not found.
+		 if(linkedListObejct.find(30))
+		 {
+			 System.out.println("\nSearch element found.");
+		 }
+		 else
+		 {
+			 System.out.println("Search element not found.");
+		 }
 	 }
 }
